a838eb81441a731f56dcef3a10740bb7e044c6a0
Avi Rana <itzavi1718@gmail.com>
2025-03-17 12:24:46 +0530

    Added zip logic

--- server/dist/routes/photoRoutes.js
+++ server/dist/routes/photoRoutes.js
@@ -7,13 +7,14 @@ const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const multer_1 = __importDefault(require("multer"));
+const adm_zip_1 = __importDefault(require("adm-zip"));
 const router = express_1.default.Router();
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
-        if (!fs_1.default.existsSync(`tmp/${req.session.user.email}/`)) {
-            fs_1.default.mkdirSync(`tmp/${req.session.user.email}/`);
+        if (!fs_1.default.existsSync(`tmp/${req.body.albumID}/`)) {
+            fs_1.default.mkdirSync(`tmp/${req.body.albumID}/`);
         }
-        cb(null, `tmp/${req.session.user.email}/`);
+        cb(null, `tmp/${req.body.albumID}/`);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + "-" + file.originalname);
@@ -24,14 +25,18 @@ router.get("/all", (req, res) => {
     try {
         let albumData = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "../../public/albums.json"), "utf-8"));
         let albumIndex = albumData.findIndex((el) => el.albumID == req.query.albumID);
-        if (albumIndex == -1) {
+        if (albumIndex == -1 || !fs_1.default.existsSync(path_1.default.join(__dirname, `../../zips/${req.query.albumID}.zip`))) {
             res.json({ type: "ERROR", msg: "AlbumID is invalid!" });
         }
         else if (albumData[albumIndex].members.findIndex((el) => el.email == req.session.user.email) == -1) {
             res.json({ type: "ERROR", msg: "Unauthorized access to album, you are not a member." });
         }
         else {
-            res.json({ type: "SUCCESS" });
+            // 
+            res.sendFile(path_1.default.join(__dirname, `../../zips/${req.query.albumID}.zip`), (err) => {
+                if (err)
+                    console.log(err);
+            });
         }
     }
     catch (err) {
@@ -40,7 +45,43 @@ router.get("/all", (req, res) => {
 });
 router.post("/new", upload.array("photos", 100), (req, res) => {
     try {
-        res.json({ type: "SUCCESS", msg: `Successfully uploaded ${req.files.length}!` });
+        let albumData = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "../../public/albums.json"), "utf-8"));
+        let albumIndex = albumData.findIndex((el) => el.albumID == req.body.albumID);
+        if (albumIndex == -1) {
+            res.json({ type: "ERROR", msg: "AlbumID is invalid!" });
+            return;
+        }
+        else if (albumData[albumIndex].members.findIndex((el) => el.email == req.session.user.email) == -1) {
+            res.json({ type: "ERROR", msg: "Unauthorized access to album, you are not a member." });
+            return;
+        }
+        (async () => {
+            if (!fs_1.default.existsSync(`tmp/${req.body.albumID}/`)) {
+                fs_1.default.mkdirSync(`tmp/${req.body.albumID}/`);
+            }
+            if (fs_1.default.existsSync(path_1.default.join(__dirname, `../zips/${req.body.albumID}.zip`))) {
+                const extractPath = path_1.default.join(__dirname, `../zips/${req.body.albumID}.zip`);
+                const inputZip = new adm_zip_1.default(extractPath);
+                inputZip.extractAllTo(extractPath, false);
+            }
+            else {
+                fs_1.default.writeFile(path_1.default.join(__dirname, `../zips/${req.body.albumID}.zip`), "", (err) => {
+                    if (err)
+                        console.log("ERROR in making file");
+                });
+            }
+            const outputZip = new adm_zip_1.default();
+            let outputPath = path_1.default.join(__dirname, `../../zips/${req.body.albumID}.zip`);
+            let inputPath = path_1.default.join(__dirname, `../../tmp/${req.body.albumID}`);
+            outputZip.addLocalFolder(inputPath);
+            outputZip.writeZip(outputPath);
+            fs_1.default.rm(path_1.default.join(__dirname, `../tmp/${req.body.albumID}/`), { recursive: true, force: true }, (err) => {
+                if (err)
+                    console.log("Error in deleting dir", err);
+            });
+            console.log("Created zip file!");
+        })();
+        res.json({ type: "SUCCESS", msg: `Successfully uploaded ${req.files.length} photos!` });
     }
     catch (err) {
         res.json({ type: "ERROR", msg: err });
--- server/src/routes/photoRoutes.ts
+++ server/src/routes/photoRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router, Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 import multer from "multer";
-import archiver from "archiver"
+import AdmZip from "adm-zip";
 
 import { Album } from "../interfaces/album.interface";
 import { AlbumMember } from "../interfaces/albumMember.interface";
@@ -10,10 +10,10 @@ import { AlbumMember } from "../interfaces/albumMember.interface";
 const router:Router = express.Router();
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        if(!fs.existsSync(`tmp/${req.session.user.email}/`)) {
-            fs.mkdirSync(`tmp/${req.session.user.email}/`);
+        if(!fs.existsSync(`tmp/${req.body.albumID}/`)) {
+            fs.mkdirSync(`tmp/${req.body.albumID}/`);
         }
-        cb(null, `tmp/${req.session.user.email}/`)
+        cb(null, `tmp/${req.body.albumID}/`)
     },
     filename: function(req, file, cb) {
         cb(null, Date.now() + "-" + file.originalname)
@@ -26,12 +26,16 @@ router.get("/all", (req:Request, res: Response)=> {
         let albumData = JSON.parse(fs.readFileSync(path.join(__dirname, "../../public/albums.json"), "utf-8"));
         let albumIndex = albumData.findIndex((el:Album)=>el.albumID==req.query.albumID);
         
-        if(albumIndex==-1) {
+        if(albumIndex==-1 || !fs.existsSync(path.join(__dirname, `../../zips/${req.query.albumID}.zip`))) {
             res.json({type: "ERROR", msg: "AlbumID is invalid!"});
         } else if(albumData[albumIndex].members.findIndex((el:AlbumMember)=>el.email==req.session.user.email)==-1) {
             res.json({type: "ERROR", msg: "Unauthorized access to album, you are not a member."});
         } else {
-            res.json({type: "SUCCESS"});
+            // 
+            res.sendFile(path.join(__dirname, `../../zips/${req.query.albumID}.zip`), (err)=>{
+                if(err) console.log(err);
+                
+            });
         }
     } catch (err) {
         res.json({type: "ERROR", msg: err});
@@ -40,6 +44,44 @@ router.get("/all", (req:Request, res: Response)=> {
 
 router.post("/new", upload.array("photos", 100), (req:Request, res:Response)=>{
     try {
+
+        let albumData = JSON.parse(fs.readFileSync(path.join(__dirname, "../../public/albums.json"), "utf-8"));
+        let albumIndex = albumData.findIndex((el:Album)=>el.albumID==req.body.albumID);
+
+        if(albumIndex==-1) {
+            res.json({type: "ERROR", msg: "AlbumID is invalid!"});
+            return;
+        } else if(albumData[albumIndex].members.findIndex((el:AlbumMember)=>el.email==req.session.user.email)==-1) {
+            res.json({type: "ERROR", msg: "Unauthorized access to album, you are not a member."});
+            return;
+        } 
+
+        (async ()=>{
+            if(!fs.existsSync(`tmp/${req.body.albumID}/`)) {
+                fs.mkdirSync(`tmp/${req.body.albumID}/`);
+            }
+            if(fs.existsSync(path.join(__dirname, `../zips/${req.body.albumID}.zip`))) {
+                const extractPath = path.join(__dirname, `../zips/${req.body.albumID}.zip`);
+                const inputZip = new AdmZip(extractPath);
+                inputZip.extractAllTo(extractPath, false);
+            } else {
+                fs.writeFile(path.join(__dirname, `../zips/${req.body.albumID}.zip`), "", (err)=>{
+                    if(err)
+                    console.log("ERROR in making file");
+                });
+            }
+            
+            const outputZip = new AdmZip();
+            let outputPath = path.join(__dirname, `../../zips/${req.body.albumID}.zip`);
+            let inputPath = path.join(__dirname, `../../tmp/${req.body.albumID}`);
+            outputZip.addLocalFolder(inputPath);
+            outputZip.writeZip(outputPath);
+            fs.rm(path.join(__dirname, `../tmp/${req.body.albumID}/`), { recursive: true, force: true }, (err)=>{
+                if(err)
+                    console.log("Error in deleting dir", err);
+            });
+            console.log("Created zip file!");
+        })();
         res.json({type: "SUCCESS", msg: `Successfully uploaded ${req.files.length} photos!`})
     } catch(err) {
         res.json({type: "ERROR", msg: err});
