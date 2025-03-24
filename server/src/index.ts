ad11dddabb34e40f1195525f1f3e3288be29fd85
Avi Rana <itzavi1718@gmail.com>
2025-03-24 23:35:32 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,8 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-    credentials: true
+    credentials: true,
+    origin: "http://localhost:8081/"
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
