576b89bd95ea7c8a1cf84b4615d6d6e3a61aad08
Avi Rana <itzavi1718@gmail.com>
2025-09-17 23:58:01 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import session from 'express-session';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({ credentials: true, origin: 'https://nook-pwa.vercel.app' });
 
+  app.set('trust proxy', 1);
   app.use(
     session({
       secret: process.env.SESSION_SECRET ?? 'my-secret',
@@ -17,13 +19,12 @@ async function bootstrap() {
       cookie: {
         httpOnly: true,
         secure: true, // secure cookies only in prod
-        sameSite: "none", // or 'none' if cross-site with secure:true
+        sameSite: 'none', // or 'none' if cross-site with secure:true
       },
     }),
   );
 
-  console.log(join(__dirname,  '../files'));
-  
+  console.log(join(__dirname, '../files'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
